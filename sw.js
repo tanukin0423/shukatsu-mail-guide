9b72272b6da0f073d5496bfdf4d7f949f5f6d6b0
tanukin0423 <166939283+tanukin0423@users.noreply.github.com>
2026-06-26 10:21:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,32 @@
-const CACHE_NAME = 'shukatsu-mail-v1';
+const CACHE_NAME = 'shukatsu-mail-v2';
+const BASE = '/shukatsu-mail-guide/';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  BASE,
+  BASE + 'index.html',
+  BASE + 'manifest.json',
+  BASE + 'icons/icon-192.png',
+  BASE + 'icons/icon-512.png',
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
   );
 });
